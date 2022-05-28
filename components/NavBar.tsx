a3d495a7068f6a9a8c136ee75a2749f8823d01c5
Kristi <kristiana.d.ingco@gmail.com>
2022-05-27 21:44:40 -0700

    Add Links to Navbar.js

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const NavBar: FunctionComponent<{}> = () => {
   return (
@@ -14,30 +15,46 @@ const NavBar: FunctionComponent<{}> = () => {
       </div>
 
       <div className="flex self-center gap-x-5">
-        <Image
-          src={"/static/icon-nav-home.svg"}
-          alt="Home Nav Icon"
-          width="16px"
-          height="16px"
-        />
-        <Image
-          src={"/static/icon-nav-movies.svg"}
-          alt="Movie Nav Icon"
-          width="16px"
-          height="16px"
-        />
-        <Image
-          src={"/static/icon-nav-tv-series.svg"}
-          alt="TV Series Nav Icon"
-          width="16px"
-          height="16px"
-        />
-        <Image
-          src={"/static/icon-nav-bookmark.svg"}
-          alt="Bookmark Nav Icon"
-          width="16px"
-          height="16px"
-        />
+        <Link href="/">
+          <a>
+            <Image
+              src={"/static/icon-nav-home.svg"}
+              alt="Home Nav Icon"
+              width="16px"
+              height="16px"
+            />
+          </a>
+        </Link>
+        <Link href="/movies">
+          <a>
+            <Image
+              src={"/static/icon-nav-movies.svg"}
+              alt="Movie Nav Icon"
+              width="16px"
+              height="16px"
+            />
+          </a>
+        </Link>
+        <Link href="/tv-series">
+          <a>
+            <Image
+              src={"/static/icon-nav-tv-series.svg"}
+              alt="TV Series Nav Icon"
+              width="16px"
+              height="16px"
+            />
+          </a>
+        </Link>
+        <Link href="/bookmarks">
+          <a>
+            <Image
+              src={"/static/icon-nav-bookmark.svg"}
+              alt="Bookmark Nav Icon"
+              width="16px"
+              height="16px"
+            />
+          </a>
+        </Link>
       </div>
 
       <div className="flex border-2 rounded-full">
